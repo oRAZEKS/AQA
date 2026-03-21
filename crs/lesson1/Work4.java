ba975f77aad745b75e79d61c6c9078578e259e52
vladlukashov <vlad.lukashov111@ya.ru>
2026-03-21 22:37:22 +0300

    ДЗ

---
+++ crs/lesson1/Work4.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class Work4 {
+    public static void main(String[] args){
+
+        System.out.println("byte: от " + Byte.MIN_VALUE + " до " + Byte.MAX_VALUE);
+        System.out.println("short: от " + Short.MIN_VALUE + " до " + Short.MAX_VALUE);
+        System.out.println("int: от " + Integer.MIN_VALUE + " до " + Integer.MAX_VALUE);
+        System.out.println("long: от " + Long.MIN_VALUE + " до " + Long.MAX_VALUE);
+        System.out.println("float: от " + Float.MIN_VALUE + " до " + Float.MAX_VALUE);
+        System.out.println("double: от " + Double.MIN_VALUE + " до " + Double.MAX_VALUE);
+        System.out.println("char: от " + Character.MIN_VALUE + " до " + Character.MAX_VALUE);
+        System.out.println("boolean - будет ошибка");
+    }
+}
---
+++ crs/lesson1/Work5.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class Work5 {
+    static public void main(String[] args){
+        String[] month = {
+                "Январь", "Февраль", "Март", "Апрель",
+                "Май", "Июнь", "Июль", "Август",
+                "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+        };
+        int birthMonthIndex = 11;
+        System.out.println("Я родился в " + month[birthMonthIndex]);
+    }
+}
