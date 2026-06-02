ae7eaae8aed98f817f0ecb4fdd63640b036fd59a
oRAZEKS <vlad.lukashov111@ya.ru>
2026-06-03 01:45:22 +0300

    Сначала протестируйте домашнее задание (#14)
    * ДЗ часть 4
    
    * ДЗ часть 4
    
    * ДЗ часть 4
    
    * ДЗ часть 4
    
    * ДЗ часть 4
    
    * ДЗ часть 4
    
    * ДЗ часть 5
    
    * ДЗ часть 5
    
    * ДЗ часть 5
    
    * ДЗ часть 6
    
    * ДЗ часть 6
    
    * ДЗ часть 6
    
    * Курс автоматизации с 0 , дз 1

---
+++ crs/NewLesson1/Division.java
@@ -0,0 +1,11 @@
+package NewLesson1;
+
+public class Division {
+    static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        int c = a/b;
+
+        System.out.println(c);
+    }
+}
